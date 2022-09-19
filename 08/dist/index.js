54cec500a95f7d5f3b13b19331bd03f27df25551
Adrion10 <lacehilaQgmail.com>
2022-09-19 19:48:13 +0200

    typs number

--- 08/dist/index.js
+++ 08/dist/index.js
@@ -2,3 +2,11 @@
 // let year: number
 // year = 2022
 // console.log(year);
+let cost = 20.22;
+//bineri number
+let myBits = 0B100;
+let myOctals = 0O100;
+let myHexadecimals = 0X100;
+let bigInt = 9007199254740991n;
+console.log(myBits, myOctals, myHexadecimals, bigInt);
+console.log(cost);
--- 08/src/index.ts
+++ 08/src/index.ts
@@ -2,3 +2,18 @@
 // year = 2022
 
 // console.log(year);
+
+let cost: number = 20.22
+
+//bineri number
+let myBits: number = 0B100
+let myOctals: number = 0O100
+let myHexadecimals: number = 0X100
+
+let bigInt: bigint = 9007199254740991n
+
+
+console.log(myBits, myOctals, myHexadecimals, bigInt);
+
+
+console.log(cost);
