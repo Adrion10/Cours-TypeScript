753a072e1bd117db06e3cedd612a075301445755
Adrion10 <lacehilaQgmail.com>
2022-09-19 17:17:34 +0200

    Type Inference

--- typescript/04/index.ts
+++ typescript/04/index.ts
@@ -1,9 +1,9 @@
 let myName ='Lace'
 console.log(typeof myName);
 let year1 
-year1= 1990
+year1= 2022
 year1='abc'
 console.log(typeof year1);
 //Type Inference
-let guest = ['Noel', 'Marisola', 'Lace',22, true]
+let guest = ['Noel', 'Marisola', 'Lace',2022, true]
 console.log(typeof guest);
