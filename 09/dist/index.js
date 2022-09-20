5d73d84f4c6ba6437a169fc185d36c02fd69a394
Adrion10 <lacehilaQgmail.com>
2022-09-20 10:54:54 +0200

    Value of Number

--- 09/dist/index.js
+++ 09/dist/index.js
@@ -3,14 +3,14 @@
 let value = 1000; //primitive type
 let otherValue = new Number(2000); // Number Object
 // console.log(value, otherValue);
-console.log(Number.EPSILON);
-console.log(Number.MAX_SAFE_INTEGER);
-console.log(Number.MAX_VALUE);
-console.log(Number.MIN_SAFE_INTEGER);
-console.log(Number.MIN_VALUE);
-console.log(Number.NEGATIVE_INFINITY);
-console.log(Number.NaN);
-console.log(Number.POSITIVE_INFINITY);
+console.log(Number.EPSILON);//2.220446049250313e-16
+console.log(Number.MAX_SAFE_INTEGER);//9007199254740991
+console.log(Number.MAX_VALUE);//1.7976931348623157e+308
+console.log(Number.MIN_SAFE_INTEGER);//-9007199254740991
+console.log(Number.MIN_VALUE);//5e-324
+console.log(Number.NEGATIVE_INFINITY);//-Infinity
+console.log(Number.NaN);//NaN  not a number
+console.log(Number.POSITIVE_INFINITY);//Infinity
 
 
 
