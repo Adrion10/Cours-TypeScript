2c3789f7dc36a2daed0ddc8d99ac330c64740d5e
Adrion10 <lacehilaQgmail.com>
2022-09-20 10:49:34 +0200

    value of Number

--- 09/dist/index.js
+++ 09/dist/index.js
@@ -1,5 +1,17 @@
 "use strict";
 // number Vs Number
 let value = 1000; //primitive type
-let otherValue = new Number(1000); // Number Object
-console.log(value, otherValue);
+let otherValue = new Number(2000); // Number Object
+// console.log(value, otherValue);
+console.log(Number.EPSILON);
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_SAFE_INTEGER);
+console.log(Number.MIN_VALUE);
+console.log(Number.NEGATIVE_INFINITY);
+console.log(Number.NaN);
+console.log(Number.POSITIVE_INFINITY);
+
+
+
+
