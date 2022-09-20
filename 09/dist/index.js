249fa4b3e2011908ef73994a3cf9a017b51b02da
Adrion10 <lacehilaQgmail.com>
2022-09-20 10:44:10 +0200

    number Vs Number

---
+++ 09/dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+// number Vs Number
+let value = 1000; //primitive type
+let otherValue = new Number(1000); // Number Object
+console.log(value, otherValue);
---
+++ 09/src/index.ts
@@ -0,0 +1,6 @@
+// number Vs Number
+
+let value = 1000 //primitive type
+let otherValue = new Number(1000)// Number Object
+
+console.log(value, otherValue);
