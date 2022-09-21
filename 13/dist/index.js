773d7b42ea5e5ef28d160377f5966b000432b491
Adrion10 <lacehilaQgmail.com>
2022-09-21 21:00:21 +0200

    object type

---
+++ 13/dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+//Object type
+let fruit;
+fruit = {
+    name: "mango",
+    taste: "Sweet",
+    color: "Yellow",
+};
+console.log(fruit);
---
+++ 13/src/index.ts
@@ -0,0 +1,8 @@
+//Object type
+let fruit: object;
+fruit = {
+  name: "mango",
+  taste: "Sweet",
+  color: "Yellow",
+};
+console.log(fruit);
