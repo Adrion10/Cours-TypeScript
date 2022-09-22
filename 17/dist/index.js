563f11432cc127dfb59fc8859a74d3c3d5641925
Adrion10 <lacehilaQgmail.com>
2022-09-22 19:00:47 +0200

    price?: number Optional parameter

---
+++ 17/dist/index.js
@@ -0,0 +1,14 @@
+"use strict";
+//fruit; //: object;
+let fruit1 = {
+    name: "mango",
+    taste: "Sweet",
+    color: "Yellow",
+    allSeason: false,
+};
+let fruit2 = {
+    name: "orange",
+    taste: "Sweet-tart",
+    color: "Orange",
+    allSeason: false,
+};
---
+++ 17/src/index.ts
@@ -0,0 +1,23 @@
+//Optional Properties
+
+type Fruit = {
+  name: string;
+  taste: string;
+  color: string;
+  allSeason: boolean;
+  price?: number;
+};
+
+let fruit1: Fruit = {
+  name: "mango",
+  taste: "Sweet",
+  color: "Yellow",
+  allSeason: false,
+  price: 10,
+};
+let fruit2: Fruit = {
+  name: "orange",
+  taste: "Sweet-tart",
+  color: "Orange",
+  allSeason: false,
+};
